81bf5403268befbfc0c143f912220d24b6a0621d
Galder Zamarreño <galder@zamarreno.com>
2019-11-06 15:11:59 +0100

    Use shared library to find out max unbounded memory of a pod

---
+++ demo-exec/demo-exec.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/galderz/golfounded/k8"
+	"github.com/galderz/golfounded/util"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	podName := "example-infinispan-0"
+	namespace := "local-operators"
+
+	kubernetes := k8.NewKubernetesFromLocalConfig()
+
+	printMaxUnboundedMemoryBytes(podName, namespace, kubernetes)
+}
+
+func printMaxUnboundedMemoryBytes(podName string, namespace string, kubernetes *k8.Kubernetes) {
+	command := []string{"cat", "/proc/meminfo"}
+
+	execOptions := k8.ExecOptions{Command: command, PodName: podName, Namespace: namespace}
+	execOut, execErr, err := kubernetes.Execute(execOptions)
+	if err == nil {
+		result := execOut.String()
+		lines := strings.Split(result, "\n")
+		for _, line := range lines {
+			if strings.Contains(line, "MemTotal:") {
+				tokens := strings.Fields(line)
+				maxUnbound, err := strconv.ParseUint(tokens[1], 10, 64)
+				util.ExpectNoError(err)
+				fmt.Printf("Max unbounded memory: %d", maxUnbound * 1024)
+				return
+			}
+		}
+	}
+	err = fmt.Errorf("unexpected error getting max unbounded memory, stderr: %v, err: %v", execErr, err)
+	util.ExpectNoError(err)
+}
